0d3b67ea52d06adb9e20f568150e513ad49b84a0
Jian Zhang <zhangjian.kims@gmail.com>
2019-03-26 13:48:09 +0800

    list列表学习

---
+++ lib/model/post.dart
@@ -0,0 +1,61 @@
+class Post {
+  
+  const Post({
+    this.title,
+    this.author,
+    this.imageUrl,
+  });
+
+  final String title;
+  final String author;
+  final String imageUrl;
+
+}
+
+final List<Post> posts =[
+  Post(
+    title: "Title 1",
+    author: "Jason",
+    imageUrl: "https://ws2.sinaimg.cn/large/9150e4e5gy1g1cuypftzwj20ci08u3z0.jpg"
+  ),
+  Post(
+    title: "Title 2",
+    author: "Jason",
+    imageUrl: "https://ws2.sinaimg.cn/large/9150e4e5gy1g1cuyrcy53j20ci08twf3.jpg"
+  ),
+  Post(
+    title: "Title 3",
+    author: "Jason",
+    imageUrl: "https://ws2.sinaimg.cn/large/9150e4e5gy1g1cuyt9nrhj20ci08uaan.jpg"
+  ),
+  Post(
+    title: "Title 4",
+    author: "Jason",
+    imageUrl: "https://ws4.sinaimg.cn/large/9150e4e5gy1g1cuyv3mgcj20ci08u3z2.jpg"
+  ),
+  Post(
+    title: "Title 5",
+    author: "Jason",
+    imageUrl: "https://ws2.sinaimg.cn/large/9150e4e5gy1g1cuywykzuj20ci08sdge.jpg"
+  ),
+  Post(
+    title: "Title 6",
+    author: "Jason",
+    imageUrl: "https://ws4.sinaimg.cn/large/9150e4e5gy1g1cuyyyiawj20ci08vdgn.jpg"
+  ),
+  Post(
+    title: "Title 7",
+    author: "Jason",
+    imageUrl: "https://ws1.sinaimg.cn/large/9150e4e5gy1g1cuz0ryh0j20ci08rq3f.jpg"
+  ),
+  Post(
+    title: "Title 8",
+    author: "Jason",
+    imageUrl: "https://ws1.sinaimg.cn/large/9150e4e5gy1g1cuz4dxq2j20ci08sdg8.jpg"
+  ),
+  Post(
+    title: "Title 9",
+    author: "Jason",
+    imageUrl: "https://ws2.sinaimg.cn/large/9150e4e5gy1g1cuz6aug1j20ci08vgm3.jpg"
+  ),
+];
